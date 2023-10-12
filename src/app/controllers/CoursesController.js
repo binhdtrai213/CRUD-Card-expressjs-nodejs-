e8161fdf00cdb82750d79771421c121260eef290
thaoneh2h <binhthan13579113@gmail.com>
2023-10-13 00:11:49 +0700

    ADD - delete route

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -36,6 +36,13 @@ class CoursesController {
             .then(() => res.redirect('/'))
             .catch(next);
     }
+
+    //[DELETE] /courses/:id/delete
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new CoursesController;
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -6,6 +6,7 @@ const coursesController = require('../app/controllers/CoursesController');
 router.get('/create', coursesController.create);
 router.post('/store', coursesController.store);
 router.get('/:id/edit', coursesController.edit);
+router.delete('/:id/delete', coursesController.delete);
 router.put('/:id', coursesController.update);
 router.get('/:slug', coursesController.show);
 
